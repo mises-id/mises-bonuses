43982337dd4d07960f82263f3773f8db7b9df163
freshlife001 <>
2023-11-07 15:00:53 +0800

    fix

--- src/pages/Mining/index.tsx
+++ src/pages/Mining/index.tsx
@@ -1,5 +1,5 @@
 import { fetchAdMiningData, reportAds, signin } from '@/api';
-//import { usePageValue } from '@/components/pageProvider';
+import { usePageValue } from '@/components/pageProvider';
 import { getSwapLink, getToken, removeToken, setToken, shortenAddress } from '@/utils';
 import { useBoolean, useDocumentVisibility, useRequest } from 'ahooks';
 import { Button, CenterPopup, Image, Toast } from 'antd-mobile'
@@ -226,7 +226,7 @@ function Mining() {
     }
   }
 
-  //const { accountData } = usePageValue()
+  const { accountData } = usePageValue()
 
   const { data: adMiningData, run, refresh, error } = useRequest(fetchAdMiningData, {
     retryCount: 3,
